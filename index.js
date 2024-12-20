22562de9f2301f4f59598310c9a5d7ced7bb2d10
Kibet Kipsang <denniskipsang2000@gmail.com>
2024-12-20 13:14:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+// const input = document.getElementById('button');
+// function clickAlert(){
+//     alert('Hey that tickles')
+// }
+// input.addEventListener('click', clickAlert)
+
+function addingEventListener(){
+    const input = document.getElementById('button');
+    input.addEventListener('click', () => alert ('Hey that tickles'))
 }
